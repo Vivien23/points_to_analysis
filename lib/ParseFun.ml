33b636b78c6e8250b0fac91d85e7cda96ffa5882
VivienG <gachet.vivien@gmail.com>
2025-05-13 16:41:31 +0200

    Removed warnings from tests

--- lib/ParseFun.ml
+++ lib/ParseFun.ml
@@ -1,6 +1,6 @@
 
 let print_ast () =
-  let crate = GetAST.load_file "test/double_free.llbc" in
+  let crate = GetAST.load_file "test/own_cf.llbc" in
   let _ = print_string (Charon.PrintLlbcAst.Crate.crate_to_string crate) in
   ()
 ;;
