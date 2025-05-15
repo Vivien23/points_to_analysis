b51608f0f8cf9fda04ff75798dfe61c0b197c188
VivienG <gachet.vivien@gmail.com>
2025-05-15 17:21:19 +0200

    print_statements now print the name of the AST node

--- lib/Printer.ml
+++ lib/Printer.ml
@@ -1,3 +1,4 @@
+let print_fake_read = false;;
 
 let print_fun_names filename =
   let crate = GetAST.load_file filename in
@@ -13,20 +14,25 @@ let print_statements filename =
 
   let behaviour_on_statement () (statement: Charon.Generated_UllbcAst.statement) =
       match statement.content with
-      | Assign _ -> ()
-      | Call _ -> ()
-      | FakeRead _ -> ()
-      | SetDiscriminant _ -> ()
-      | StorageDead _ -> ()
-      | Deinit _ -> ()
-      | Drop _ -> ()
-      | Assert _ -> ()
-      | Nop -> ()
+      | Assign (place, _rvalue) -> 
+          let assign_to = 
+            match place.kind with
+            | PlaceBase var_id -> Charon.PrintExpressions.var_id_to_pretty_string var_id
+            | PlaceProjection (_place, _proj_elem) -> "TODO : projections"
+          in print_string "Assign "; print_string "smt"; print_string " to "; print_endline assign_to
+      | Call _ -> print_endline "Call"
+      | FakeRead _ -> if print_fake_read then print_endline "FakeRead"
+      | SetDiscriminant _ -> print_endline "SetDiscriminant"
+      | StorageDead _ -> print_endline "StorageDead"
+      | Deinit _ -> print_endline "Deinit"
+      | Drop _ -> print_endline "Drop"
+      | Assert _ -> print_endline "Assert"
+      | Nop -> print_endline "Nop"
   in
 
   let behaviour_on_fun_decl _ (fun_decl: Charon.UllbcAst.blocks Charon.UllbcAst.gfun_decl) =
     match fun_decl.body with
-    | Some(bdy: Charon.UllbcAst.blocks Charon.UllbcAst.gexpr_body) -> List.fold_left (fun _ (block: Charon.UllbcAst.block)  -> List.fold_left behaviour_on_statement () block.statements) () (bdy.body)
+    | Some(bdy: Charon.UllbcAst.blocks Charon.UllbcAst.gexpr_body) -> List.fold_left (fun _ (block: Charon.UllbcAst.block)  -> print_endline "Entering new block :"; List.fold_left behaviour_on_statement () block.statements; print_endline "") () (bdy.body)
     | None -> ()
   in 
 
---
+++ test/rust/src/loop.rs
@@ -0,0 +1,9 @@
+//Testcase for widening
+
+fn main(){
+    let x = 0; //Todo : make x a list of borrows or smth
+    for i in 0..100 {
+        let x = x;
+    }
+      
+}
