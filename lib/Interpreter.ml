5ca0cdb96abb9d19d837c6c02b4f031518d0d534
VivienG <gachet.vivien@gmail.com>
2025-05-27 14:38:19 +0200

    Changed the signature of Borrow from place_kind * borrow_kind to place * borrow_kind

--- lib/Interpreter.ml
+++ lib/Interpreter.ml
@@ -10,7 +10,7 @@ end)
 
 type value = 
   | Cons of Charon.Values.big_int
-  | Borrow of place_kind * borrow_kind
+  | Borrow of place * borrow_kind
   | Bottom
 
 type expression =
@@ -51,14 +51,15 @@ let to_expr (rvalue: rvalue): expression =
 ;;
 
 let interpreter crate = 
+  (* For each block of a crate, interprets its content and prints the memory at each program point*)
   let rec place_helper (sigma: value VarIdMap.t) place counter : var_id =
     match place, counter with
     | PlaceBase var_id, 0 -> var_id
     | PlaceBase (var_id: var_id), _ -> assert (counter > 0); 
       (match VarIdMap.find var_id sigma with
       | Cons _ -> failwith "Illegal dereference"
-      | Borrow(p_kind, _b_kind) -> (match p_kind with                                (* TODO : pay attention to the borrow_kind*)
-        | PlaceBase _ -> place_helper sigma p_kind (counter - 1)
+      | Borrow(place, _b_kind) -> (match place.kind with                                (* TODO : pay attention to the borrow_kind*)
+        | PlaceBase _ -> place_helper sigma place.kind (counter - 1)
         | PlaceProjection(_place, Deref) -> failwith "Double borrow without local deteced"
         | PlaceProjection(_place, Field _) -> failwith "Fields aren't supported yet")
       | Bottom -> failwith "Using a dropped value")
@@ -67,27 +68,37 @@ let interpreter crate =
   in
 
   let rec interpret_expr (sigma: value VarIdMap.t) (expr: expression) =
-    let temp_res = sigma, Cons(Z.of_int 42) in
     match expr with
     | Val(Cons cst) -> sigma, (Cons cst)
-    | Val(Borrow(_)) -> failwith "Double borrow without local deteced"
-      (* TODO : turn this into a better error message
-      The following code works if we track the place instead of place_kind in Borrow
-      let place = Charon.PrintExpressions.place_to_string (Charon.PrintUtils.of_crate crate) place in
-      Printf.eprintf "Expected a constant, found %s" place; raise Plonk *)
+    | Val(Borrow(place, _b_kind)) -> 
+      let p_string = Charon.PrintExpressions.place_to_string (Charon.PrintUtils.of_crate crate) place in
+      Printf.eprintf "Expected a constant, found %s" p_string; raise Plonk 
+    | Val(Bottom) -> raise Plonk
     | Copy(place) -> let var_id = place_helper sigma place.kind 0 in sigma, VarIdMap.find var_id sigma
     | Move(place)  -> 
       (match place.kind with
       | PlaceBase var_id -> let temp = VarIdMap.find var_id sigma in VarIdMap.add var_id Bottom sigma, temp
       | _ -> failwith "Complicated move that involves ownership")
-    | Borrow(place, b_kind) -> let var_id = place_helper sigma place.kind 0 in sigma, Borrow(PlaceBase(var_id), b_kind)
-    | _ -> Printf.eprintf "Pattern matching failed on "; print_expression ~channel:stderr crate expr ; raise Plonk
+    | Borrow(place, b_kind) -> let var_id = place_helper sigma place.kind 0 in
+      sigma, Borrow({kind = PlaceBase(var_id); ty = place.ty}, b_kind)
   in
 
   let interpret_stm (sigma: value VarIdMap.t) (stm: Charon.UllbcAst.statement) =
   match stm.content with
   | Assign(place, rvalue) -> let var_id = place_helper sigma place.kind 0 in 
-    let sigma', value = interpret_expr sigma rvalue
+    let sigma', value = interpret_expr sigma (to_expr rvalue) in VarIdMap.add var_id value sigma'
+
+  | StorageDead _ -> Printf.eprintf "TODO : Storage dead"; sigma
+  | Drop _ -> Printf.eprintf "TODO : Drop"; sigma
+
+  | FakeRead _ -> sigma
+  | Nop -> sigma
+
+  | _ -> failwith "Non exhaustive pattern"
   in
-  ()
+  
+  Charon.Types.FunDeclId.Map.iter (fun _ (fun_decl: Charon.UllbcAst.blocks Charon.UllbcAst.gfun_decl) -> 
+    match fun_decl.body with | Some(bdy: Charon.UllbcAst.blocks Charon.UllbcAst.gexpr_body) -> ()
+                             | None -> ()
+  ) crate.fun_decls
 ;;
